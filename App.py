73e1f43dd1e1191c41e01645b122034a4a778002
Temirlan Yesmagambet <tesmagambet2005@gmail.com>
2024-11-20 00:31:22 +0500

    update

--- App.py
+++ App.py
@@ -1,28 +1,43 @@
-
 import streamlit as st
+from rich.jupyter import display
+
 from Pages import Home, Project1, Project2, Project3
 from streamlit_navigation_bar import st_navbar as navbar
+from PIL import Image
 
-st.set_page_config(initial_sidebar_state="collapsed")
+image = Image.open("img/icons8-berserk-48.png")
+import pandas as pd
+import numpy as np
+
+st.set_page_config(initial_sidebar_state="collapsed", page_icon=image)
 pages = ["Home", "Project1", "Project2", "Project3"]
 styles = {
     "nav": {
-        "background-color": "rgb(123, 209, 146)",
+        "background-color": "#1a1a1a",
+        "display": "flex",
+        "justify-content": "center",
+
     },
-    "div": {
-        "max-width": "32rem",
+    "img": {
+        "position": "absolute",
+        "left": "-20px",
+        "font-size": "15px",
+        "top": "4px",
+        "width": "100px",
+        "height": "40px",
+
     },
     "span": {
         "border-radius": "0.5rem",
-        "color": "rgb(49, 51, 63)",
-        "margin": "0 0.125rem",
+        "color": "#ffffff",
+        "font-size": "14px",
         "padding": "0.4375rem 0.625rem",
     },
     "active": {
-        "background-color": "rgba(105, 114, 255, 0.25)",
+        "background-color": "#ff0000",
     },
     "hover": {
-        "background-color": "rgba(255, 255, 255, 0.35)",
+        "background-color": "rgba(255, 255, 255, 0.2)",
     }
 }
 
@@ -38,5 +53,3 @@ elif page == "Project3":
     Project3.Project3().app()
 else:
     Home.Home().app()
-
-
